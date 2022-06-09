2ec841fe3ac91cfd2c40b7d75e4df05129e4e78e
Xanul <rivas.060@gmail.com>
2022-06-09 15:07:09 -0500

    Initial commit

---
+++ src/js/componentes.js
@@ -0,0 +1,22 @@
+import '../css/componentes.css';
+// import webpackLogo from '../assets/img/webpack-logo.png'
+
+export const saludar = (nombre = "Sin Nombre") => {
+    console.log('Creando etiqueta h1');
+
+    const h1 = document.createElement('h1');
+
+    h1.innerText = `Hola, ${nombre}!!`;
+
+    document.body.append(h1);
+
+    // Img
+    // console.log(webpackLogo)
+    // const img = document.createElement('img');
+    // img.src = webpackLogo;
+    // document.body.append(img);
+
+}
+
+
+
